7bb509c08c5b964e76090498690131220166cdca
JasonNiuSDU <niuchao_uestc@163.com>
2020-02-08 19:12:51 +0800

    Update skinny2.cpp

--- skinny_64_192/skinny2.cpp
+++ skinny_64_192/skinny2.cpp
@@ -123,7 +123,7 @@ int main(int argc,char * argv[])
     //peogram main
     ofstream outcvc;
     int x_ROUND = 8;
-    int y_ROUND = 10;
+    int y_ROUND = 11;
     int ROUND = x_ROUND+y_ROUND;
 
 	P_make(ROUND);
