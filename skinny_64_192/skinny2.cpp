dc83149d9c6b4768dbb085f464ecb6bdca1e6774
nc26676027 <42799670+nc26676027@users.noreply.github.com>
2020-02-11 09:36:56 +0800

    Update skinny2.cpp

--- skinny_64_192/skinny2.cpp
+++ skinny_64_192/skinny2.cpp
@@ -122,7 +122,7 @@ int main(int argc,char * argv[])
 
     //peogram main
     ofstream outcvc;
-    int x_ROUND = 7;
+    int x_ROUND = 8;
     int y_ROUND = 10;
     int ROUND = x_ROUND+y_ROUND;
 
