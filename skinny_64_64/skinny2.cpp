dc440db5cb0b4ba59232ece664619ecf5160e0c8
JasonNiuSDU <niuchao_uestc@163.com>
2020-02-14 12:04:21 +0800

    666

--- skinny_64_64/skinny2.cpp
+++ skinny_64_64/skinny2.cpp
@@ -117,7 +117,7 @@ int main(int argc,char * argv[])
     //peogram main
     ofstream outcvc;
     int x_ROUND = 6;
-    int y_ROUND = 7;
+    int y_ROUND = 6;
     int ROUND = x_ROUND+y_ROUND;
 
 	P_make(ROUND);
