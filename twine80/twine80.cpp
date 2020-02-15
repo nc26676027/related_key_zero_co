c9fb15df7ad7b899fe9843114d6cb3523c209972
JasonNiuSDU <niuchao_uestc@163.com>
2020-02-15 15:07:05 +0800

    Update twine80.cpp

--- twine80/twine80.cpp
+++ twine80/twine80.cpp
@@ -82,8 +82,8 @@ int main(int argc,char * argv[])
 	
 	//program main
 	ofstream outcvc;
-    int x_ROUND = 7;
-	int y_ROUND = 7;
+    int x_ROUND = 8;
+	int y_ROUND = 8;
 	int ROUND = x_ROUND+y_ROUND;
 
 
