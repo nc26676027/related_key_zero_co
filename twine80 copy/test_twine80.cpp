c707a253cdb4a28e0a72faaa442dcc1b71dc82e4
JasonNiuSDU <niuchao_uestc@163.com>
2020-02-21 09:45:35 +0800

    Update test_twine80.cpp

--- twine80 copy/test_twine80.cpp
+++ twine80 copy/test_twine80.cpp
@@ -138,14 +138,56 @@ int testTK1(void)
 {
     //generate all keys at random
     srand( time(NULL));
-    vector<vector<int>> key1(3 , vector<int>(2, 0));
+
+    vector<vector<int>> alpha1(2 , vector<int>(2, 0));
+    for( int row = 0; row < 2 ; row++)
+    {
+        for( int col = 0; col < 2; col++)
+        {
+            if( (row*2+col) == 2 )
+            {
+                alpha1[row][col] = rand() & 0xF;
+            }
+            else
+            {
+                alpha1[row][col] = 0x0;
+            }   
+            
+        }
+    }
+    vector<vector<int>> alpha2(3 , vector<int>(2, 0));
     for( int row = 0; row < 3 ; row++)
     {
         for( int col = 0; col < 2; col++)
         {
-            key1[row][col] = rand() & 0xF;
+            if( (row*2+col) == 3 )
+            {
+                alpha2[row][col] = 0x0;
+            }
+            else
+            {
+                alpha2[row][col] = rand() & 0xF;
+            }   
+            
         }
     }
+    vector<vector<int>> beta(2 , vector<int>(2, 0));
+    for( int row = 0; row < 2 ; row++)
+    {
+        for( int col = 0; col < 2; col++)
+        {
+            if( (row*2+col) == 0 )
+            {
+                beta[row][col] = rand() & 0xF;
+            }
+            else
+            {
+                beta[row][col] = 0x0;
+            }   
+            
+        }
+    }
+
 
     int x_Rounds = 5;
     int y_Rounds = 4;
@@ -153,40 +195,81 @@ int testTK1(void)
     printf("  Number of rounds : %d\n" , Round);
 
     vector<int> counter(16 , 0);
+    int XOR = 0;
     for ( int i1 = 0; i1 < 16; i1++)
     {
         for ( int i2 = 0; i2 < 16; i2++)
-        {
-            vector<vector<int>> in(2, vector<int>(2, 0));
-            vector<vector<int>> tk1 = key1;
-
-
-            in[3][2] = i1;
-
-            tk1[3][1] = i2;
-
-
-            //encryption
-
-            for (int r = 0; r < Round - 1 ; r++)
+        {   
+            for ( int i3 = 0; i3 < 16; i3++)
             {
-                in = subByte (in , tk1);
-                in = shiftNible(in);
+                for ( int i4 = 0; i4 < 16; i4++)
+                {
+                    for ( int i5 = 0; i5 < 16; i5++)
+                    {
+                        for ( int i6 = 0; i6 < 16; i6++)
+                        {
+                            for ( int i7 = 0; i7 < 16; i7++)
+                            {
+                                for ( int i8 = 0; i8 < 16; i8++)
+                                {
+                                    for ( int i9 = 0; i9 < 16; i9++)
+                                    {
+                                        vector<vector<int>> in(2, vector<int>(2, 0));
+                                        vector<vector<int>> tk1(3, vector<int>(2, 0));
 
-                tk1 = keySchedule(tk1);
+                                        in[0][0] = i1;
+                                        in[0][1] = i2;
+                                        in[1][0] = i3;
+                                        in[1][1] = i4;
+                                        tk1[0][0] = i5;
+                                        tk1[0][1] = i6;
+                                        tk1[1][0] = i7;
+                                        tk1[1][1] = i8;
+                                        tk1[2][0] = i9;
+                                        
+                                        vector<vector<int>> K = tk1;
+                                        vector<vector<int>> P = in;
 
-            }
-            in = subByte (in , tk1);
+                                        //encryption
+                                        for (int r = 0; r < Round - 1 ; r++)
+                                        {
+                                            in = subByte (in , tk1);
+                                            in = shiftNible(in);
 
-            counter[in[2][0]]++;
+                                            tk1 = keySchedule(tk1);
 
+                                        }
+                                        in = subByte (in , tk1);
 
-            
+                                        XOR = ( K[0][0] & alpha2[0][0] ) ^ ( K[0][1] & alpha2[0][1] ) ^ ( K[1][0] & alpha2[1][0] ) ^ ( K[2][0] & alpha2[2][0] );
+                                        for( int i = 0; i < 2; i++)
+                                        {
+                                            for( int j =0; j < 2; j++)
+                                            {
+                                                XOR = XOR ^ ( P[i][j] & alpha1[i][j] ); 
+                                            }
+                                        }
+                                        for( int i = 0; i < 2; i++)
+                                        {
+                                            for( int j =0; j < 2; j++)
+                                            {
+                                                XOR = XOR ^ ( in[i][j] & beta[i][j] ); 
+                                            }
+                                        }                                                 
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }             
         }
     }
+
     for (int i = 0; i < 16; i++)
     {
-        printf("    %X : frequency of apprearance = %5d, mod 2 = %d \n", i , counter[i] , counter[i] % 2);
+        printf("    %X : XOR's value", XOR );
     }
     cout << endl;
 }
