97c62e048041bfe2a61cee173a12b1782ed498f2
JasonNiuSDU <niuchao_uestc@163.com>
2020-02-25 18:33:22 +0800

    Update multi_thread.sh

--- twine80 copy/multi_thread.sh
+++ twine80 copy/multi_thread.sh
@@ -6,8 +6,7 @@ cp ../test_twine80.cpp ./
 g++ --std c++11 test_twine80.cpp -o test_twine80
 for ((i=0;i<8;i++));
 do	
-	nohup ./test_twine80 $i > r_$i.txt &
-	cd ..
+	nohup ./test_twine80 $i > r_$i.txt &	
 done
 
 
