80e31e4df0c9b092fcccc77c240eba62bb7315da
JasonNiuSDU <niuchao_uestc@163.com>
2020-03-07 13:03:20 +0800

    666

---
+++ toy_cipher_zc/test.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<time.h>
+#include<cmath>
+#include <stdint.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main(void)
+{
+    printf("Experimental encrypt table on toy_cipher.\n");
+    printf("%d" , sizeof(int));
+
+    return 0;
+}
