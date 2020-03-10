4c09d9ce58f25da73e1514286f40f825a62b8727
JasonNiuSDU <niuchao_uestc@163.com>
2020-03-10 16:09:05 +0800

    Update multi_thread counter.sh

--- toy_cipher_zc/multi_thread counter.sh
+++ toy_cipher_zc/multi_thread counter.sh
@@ -3,25 +3,25 @@
 mkdir counter0_h
 cd counter0_h
 cp ../counter*.cpp ./ 
-g++ --std c++11 counterone.cpp -o counterone
-g++ --std c++11 countertwo.cpp -o countertwo
-g++ --std c++11 counterthree.cpp -o counterthree
-g++ --std c++11 counterfour.cpp -o counterfour
-g++ --std c++11 counterfive.cpp -o counterfive
-g++ --std c++11 countersix.cpp -o countersix
-g++ --std c++11 counterseven.cpp -o counterseven
-g++ --std c++11 countereight.cpp -o countereight
+g++ --std c++11 counter1.cpp -o counter1
+g++ --std c++11 counter2.cpp -o counter2
+g++ --std c++11 counter3.cpp -o counter3
+g++ --std c++11 counter4.cpp -o counter4
+g++ --std c++11 counter5.cpp -o counter5
+g++ --std c++11 counter6.cpp -o counter6
+g++ --std c++11 counter7.cpp -o counter7
+g++ --std c++11 counter8.cpp -o counter8
 
 for ((i=0;i<1;i++));
 do	
-	nohup ./counterone $i &	
-	nohup ./countertwo $i &	
-	nohup ./counterthree $i &	
-	nohup ./counterfour $i &		
-	nohup ./counterfive $i &	
-	nohup ./countersix $i &	
-	nohup ./counterseven $i &	
-	nohup ./countereight $i &	
+	nohup ./counter1 $i &	
+	nohup ./counter2 $i &	
+	nohup ./counter3 $i &	
+	nohup ./counter4 $i &		
+	nohup ./counter5 $i &	
+	nohup ./counter6 $i &	
+	nohup ./counter7 $i &	
+	nohup ./counter8 $i &	
 
 done
 cd ..
