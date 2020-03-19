ee9f75690fb039b8a44eccb8888c266455aa6e96
nc26676027 <42799670+nc26676027@users.noreply.github.com>
2020-03-19 11:04:30 +0800

    Update outresult.sh

--- toy_cipher_zc/outresult.sh
+++ toy_cipher_zc/outresult.sh
@@ -6,7 +6,7 @@ cp ../add_counter*.cpp ./
 for ((i=3;i<6;i++));
 do	
 	g++ --std c++11 add_counter$i.cpp -o add_counter$i
-	nohup ./add_counter$i > re_counter$i &	
+	nohup ./add_counter$i > re_counter$i.txt &	
 done
 cd ..
 
