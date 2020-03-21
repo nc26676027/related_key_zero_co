d2dac10ead1e9e1d62619a159bfb913a10545acf
nc26676027 <42799670+nc26676027@users.noreply.github.com>
2020-03-21 09:10:46 +0800

    Update outresult.sh

--- toy_cipher_zc/outresult.sh
+++ toy_cipher_zc/outresult.sh
@@ -3,7 +3,7 @@
 mkdir cout_re
 cd cout_re
 cp ../add_counter*.cpp ./ 
-for ((i=3;i<6;i++));
+for ((i=12;i<16;i++));
 do	
 	g++ --std c++11 add_counter$i.cpp -o add_counter$i
 	nohup ./add_counter$i > re_counter$i.txt &	
