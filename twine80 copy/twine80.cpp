05b7b0fbf265bc63ad27c535a1c47865586d81a8
nc26676027 <42799670+nc26676027@users.noreply.github.com>
2020-03-27 16:06:34 +0800

    Update twine80.cpp

--- twine80 copy/twine80.cpp
+++ twine80 copy/twine80.cpp
@@ -73,7 +73,7 @@ int main(int argc,char * argv[])
 	
 	//program main
 	ofstream outcvc;
-    int x_ROUND = 3;
+    int x_ROUND = 5;
 	int y_ROUND = 5;
 	int ROUND = x_ROUND+y_ROUND;
 
@@ -303,10 +303,10 @@ int main(int argc,char * argv[])
 				string a = "Kout_"+to_string(round)+"_"+to_string(pos); 
 				string b = "KSin_"+to_string(round)+"_"+to_string(pos);
 				outcvc<<"ASSERT( "<<a<<" = "<<b<<" );"<<endl;
-				outcvc<<"ASSERT( NOT( LAT[KSin_"<<round<<"_"<<pos<<"@KSout_"<<round<<"_"<<pos<<"] = 0bin0 ) );"<<endl;
+				//outcvc<<"ASSERT( NOT( LAT[KSin_"<<round<<"_"<<pos<<"@KSout_"<<round<<"_"<<pos<<"] = 0bin0 ) );"<<endl;
 
 				outcvc<<"ASSERT( Rotin_"<<round<<"_"<<pos<<" = KSout_"<<round<<"_"<<pos<<" );"<<endl;
-				//outcvc<<"ASSERT( KSin_"<<round<<"_"<<pos<<" = KSout_"<<round<<"_"<<pos<<" );"<<endl;
+				outcvc<<"ASSERT( KSin_"<<round<<"_"<<pos<<" = KSout_"<<round<<"_"<<pos<<" );"<<endl;
 				//outcvc<<"ASSERT( "<<"KSout_"<<round<<"_"<<pos<<" = Rotin_"<<round<<"_"<<pos+1<<" );"<<endl;
 			}
 			else
@@ -334,7 +334,7 @@ int main(int argc,char * argv[])
 
 		if( pos < 4 )
 		{
-			if(  (pos == 0) || (pos == 3)  )
+			if(  pos == head_flag  )
 			{
 				outcvc<<"ASSERT( NOT( x_Fin_0_"<<pos<<" = 0bin0000 ) );"<<endl;
 			}
@@ -342,7 +342,7 @@ int main(int argc,char * argv[])
 			{
 				outcvc<<"ASSERT( x_Fin_0_"<<pos<<" = 0bin0000 );"<<endl;
 			}
-			if(pos == 0)
+			if(pos == tail_flag)
 			{
 				outcvc<<"ASSERT( NOT( y_Xout_"<<y_ROUND-1<<"_"<<pos<<" = 0bin0000 ) );"<<endl;
 			}
@@ -355,7 +355,7 @@ int main(int argc,char * argv[])
 		
 		if( pos < 4 )
 		{
-			if( pos == 0 )
+			if( pos == key_flag )
 			{
 				outcvc<<"ASSERT( Kin_0_"<<pos<<" = 0bin0000 );"<<endl;
 			}
