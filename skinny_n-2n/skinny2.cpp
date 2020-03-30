371ea9400ac12e95134768ce8fd858cedf79fe37
nc26676027 <42799670+nc26676027@users.noreply.github.com>
2020-03-30 09:36:23 +0800

    Update skinny2.cpp

--- skinny_n-2n/skinny2.cpp
+++ skinny_n-2n/skinny2.cpp
@@ -315,7 +315,7 @@ int main(int argc,char * argv[])
 	{
 		if(pos<16)
 		{
-			if( (pos == 11) || (pos == 14) )
+			if( (pos == 11) || (pos == 15) )
 			{
 				outcvc<<"ASSERT(  x_Sin_0_"<<pos<<" = 0bin0000  );"<<endl;
 			}
