329785c33a25c8e3e00f24edc899c72e8dd7e842
nc26676027 <42799670+nc26676027@users.noreply.github.com>
2020-04-03 16:32:06 +0800

    Update LBlock.cpp

--- LBlock/LBlock.cpp
+++ LBlock/LBlock.cpp
@@ -218,8 +218,8 @@ int main(int argc,char * argv[])
 	
 	//program main
 	ofstream outcvc;
-    int x_ROUND = 6;
-	int y_ROUND = 6;
+    int x_ROUND = 8;
+	int y_ROUND = 8;
 	int ROUND = x_ROUND+y_ROUND;
 
 
