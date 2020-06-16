918ef72bd01b9c04619bc8f7bef0e2e44e3298ac
jasonNiu <jasonN@qq.com>
2020-06-16 14:11:47 +0800

    Update craft bias.cpp

--- craft/craft bias.cpp
+++ craft/craft bias.cpp
@@ -295,7 +295,7 @@ int main(int argc,char * argv[])
 
 	//outcvc<<"ASSERT( x_Fin_0_4 = x_Fin_0_12 );"<<endl;
 	//outcvc<<"ASSERT( NOT( x_Fin_0_4 = 0bin0000 ) );"<<endl;
-
+/*
 	//概率限制
 	outcvc<<"ASSERT( P = BVPLUS( 16";
 	for(int round=0;round<ROUND;round++)
@@ -307,7 +307,7 @@ int main(int argc,char * argv[])
 
 	}
 	outcvc<<" ) );"<<endl;
-
+*/
 	//outcvc<<"ASSERT( BVLE( P , 0hex00ff ) );"<<endl;
 
 
