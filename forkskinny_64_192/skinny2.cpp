11112e5906025d8c9f688ea6fb7da37da14d3702
jasonNiu <jasonN@qq.com>
2020-06-28 15:57:03 +0800

    Update skinny2.cpp

--- forkskinny_64_192/skinny2.cpp
+++ forkskinny_64_192/skinny2.cpp
@@ -117,7 +117,7 @@ int main(int argc,char * argv[])
     //peogram main
     ofstream outcvc;
     int x_ROUND = 10;
-    int y_ROUND = 16;
+    int y_ROUND = 14;
     int ROUND = x_ROUND+y_ROUND;
 
 	P_make(ROUND);
