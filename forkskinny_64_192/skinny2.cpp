696c89743f12e64a4443d01e2eb52e4f519d8dd7
jasonNiu <jasonN@qq.com>
2020-07-07 18:18:23 +0800

    Update skinny2.cpp

--- forkskinny_64_192/skinny2.cpp
+++ forkskinny_64_192/skinny2.cpp
@@ -116,8 +116,8 @@ int main(int argc,char * argv[])
 
     //peogram main
     ofstream outcvc;
-    int x_ROUND = 4;
-    int y_ROUND = 14;
+    int x_ROUND = 5;
+    int y_ROUND = 13;
 	int r0 = 23;
     int ROUND = x_ROUND+y_ROUND+r0;
 
@@ -385,9 +385,9 @@ int main(int argc,char * argv[])
 		{
 			if(pos<8)
 			{
-				outcvc<<"ASSERT( RKin_"<<x_ROUND+round<<"_"<<pos<<" = 0bin0001 );"<<endl;
-				outcvc<<"ASSERT( RKin2_"<<x_ROUND+round<<"_"<<pos<<" = 0bin0001 );"<<endl;
-				outcvc<<"ASSERT( RKin3_"<<x_ROUND+round<<"_"<<pos<<" = 0bin0001 );"<<endl;
+				outcvc<<"ASSERT( RKin_"<<x_ROUND+round<<"_"<<pos<<" = 0bin0000 );"<<endl;
+				outcvc<<"ASSERT( RKin2_"<<x_ROUND+round<<"_"<<pos<<" = 0bin0000 );"<<endl;
+				outcvc<<"ASSERT( RKin3_"<<x_ROUND+round<<"_"<<pos<<" = 0bin0000 );"<<endl;
 			}			
 		}
 	}
