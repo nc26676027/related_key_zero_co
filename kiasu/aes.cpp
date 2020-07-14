4e7efccbcb8d9c21b91d5a1112fb397724a301c5
jasonNiu <jasonN@qq.com>
2020-07-14 10:22:01 +0800

    Update aes.cpp

--- kiasu/aes.cpp
+++ kiasu/aes.cpp
@@ -352,7 +352,7 @@ int main(int argc,char * argv[])
 
 			if( pos == key_flag )
 			{
-				//outcvc<<"ASSERT( Kin_0_"<<pos<<" = 0bin00000000 );"<<endl;
+				outcvc<<"ASSERT( Kin_0_"<<pos<<" = 0bin00000000 );"<<endl;
 
 			}
 		}
